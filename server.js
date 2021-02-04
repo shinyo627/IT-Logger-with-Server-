91b7deef215e578fe998ea99e0237ed860750716
shinyo627 <shinyo627@gmail.com>
2021-02-04 19:40:56 +0100

    Ready to Deploy

--- server.js
+++ server.js
@@ -9,15 +9,21 @@ connectDB();
 // Initialize Middleware for accepting incoming req.body
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the IT-logger' });
-});
-
 // Defined Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Will be used in production
 const PORT = process.env.PORT || 5000;
